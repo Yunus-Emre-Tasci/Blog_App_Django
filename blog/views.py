14b10536816577ddb404e3938f50620087e0ad06
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-01-03 23:11:49 +0300

    permissions

--- blog/views.py
+++ blog/views.py
@@ -3,16 +3,18 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Category,Blog
 from .serializers import CategorySerializer,BlogSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsAdminOrReadOnly
 
 # Create your views here.
 class CategoryView(ModelViewSet):
     queryset=Category.objects.all()
     serializer_class=CategorySerializer
+    permission_classes=[IsAdminOrReadOnly]
     filterset_fields=["name"]
     
 class BlogView(ModelViewSet):
     queryset=Blog.objects.all()
     serializer_class=BlogSerializer
-    permission_classes=["IsAuthenticatedOrReadOnly"]
+    permission_classes=[IsAuthenticatedOrReadOnly]
     filterset_fields=["category"]
     search_fields=["title","content"]
