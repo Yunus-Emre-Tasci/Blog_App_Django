06596c98af338778f3d32600f6dbb810d5928e47
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-01-03 23:02:35 +0300

    login

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Category,Blog
 from .serializers import CategorySerializer,BlogSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # Create your views here.
 class CategoryView(ModelViewSet):
@@ -12,5 +13,6 @@ class CategoryView(ModelViewSet):
 class BlogView(ModelViewSet):
     queryset=Blog.objects.all()
     serializer_class=BlogSerializer
+    permission_classes=["IsAuthenticatedOrReadOnly"]
     filterset_fields=["category"]
     search_fields=["title","content"]
