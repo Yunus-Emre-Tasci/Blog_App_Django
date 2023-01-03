25b2b2641152353c7dd24c0696cf070630c77d6c
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-01-03 21:38:53 +0300

    filter

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,9 @@ from .serializers import CategorySerializer,BlogSerializer
 class CategoryView(ModelViewSet):
     queryset=Category.objects.all()
     serializer_class=CategorySerializer
+    filter_fields=["name"]
     
 class BlogView(ModelViewSet):
     queryset=Blog.objects.all()
     serializer_class=BlogSerializer
+    filter_fields=["category"]
--- main/settings.py
+++ main/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     "blog",
     
     #3rd parth library
-    "rest_framework"
+    "rest_framework",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
