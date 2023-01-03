6cf83d84ed2f293e5b3ce600471a5f86910f88f7
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-01-03 20:19:54 +0300

    models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=25,unique=True)
+    
+    def __str__(self):
+        return self.name
+
+class Blog(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField(blank=True)
+    category=models.ForeignKey(Category,on_delete=models.PROTECT)
+    is_published=models.BooleanField(default=False)
+    created_date=models.DateTimeField(auto_now_add=True)
+    updated_date=models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
